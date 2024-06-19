ab4cef7f8bead6111aa79de6d783f84dfe5fd911
Alexander Pechinkin <pechinkin04@yandex.ru>
2024-06-19 12:09:02 +0400

    Initial Commit

---
+++ Swift Gaze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift Gaze
+//
+//  Created by Александр Печинкин on 19.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift Gaze/Swift_GazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_GazeApp.swift
+//  Swift Gaze
+//
+//  Created by Александр Печинкин on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_GazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
